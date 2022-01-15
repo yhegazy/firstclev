eac019386dd9df1503121a3c040b0db19d37f936
yahia <hegazyx@gmail.com>
2022-01-15 21:34:42 +0000

    reverted navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import {useState} from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import {Navbar, Nav, Form} from 'react-bootstrap'
 
 const NavMenu = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -9,28 +10,20 @@ const NavMenu = () => {
     }
 
     return <>
-        <nav 
-            className="navbar" 
-            expand={'sm'}
-            onClick={() => toggle()}
-            aria-controls="basic-navbar-nav"
-            aria-expanded={isOpen}
-        >
-            
-            <div aria-controls="basic-navbar-nav" />
-            <div id="basic-navbar-nav" in={isOpen}>
-            <Link className="navbar-brand mr-auto pl-2" to="/">The First Cleveland Mosque</Link>
-                
-                <div className="mx-5">
-                    
-                    <Link className="nav-link" to="/salah">Prayer Times</Link>
-                    <Link className="nav-link" to="/archives">YouTube Archives</Link>
-                    <Link className="nav-link" to="/annoucements">Annoucements</Link>
-                    <Link className="nav-link" to="/about">About Us</Link>
-                    <Link className="nav-link" to="/covid">COVID-19</Link>
-                </div>
-        </div>
-    </nav>
+        <Navbar className="navbar fixed-top navbar-expand-md navbar-dark bg-dark" expand="lg">
+                <NavLink className="navbar-brand mx-auto" to="/">The First Cleveland Mosque</NavLink>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-5">
+                        <NavLink className="nav-link" to="/salah">Prayer Times</NavLink>
+                        <NavLink className="nav-link" to="/archives">YouTube Archives</NavLink>
+                        <NavLink className="nav-link" to="/annoucements">Annoucements</NavLink>
+                        <NavLink className="nav-link" to="/about">About Us</NavLink>
+                        <NavLink className="nav-link" to="/covid">COVID-19</NavLink>
+                    </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     </>
 }
 
