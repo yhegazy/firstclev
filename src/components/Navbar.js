c8a40ed61abe768633ddb7e5128b5e7e38ed13d7
yhegazy <hegazyx@gmail.com>
2022-08-14 16:25:23 -0400

    20220814::Navbar updated. Less millenial more boomer.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,80 +1,107 @@
 import {Link} from 'react-router-dom'
+import {useState} from 'react'
 
 const menuItems = [
     {
         id: 1,
         title: 'Home',
         exact: true,
-        to: '/',
-        icon: 'fa-solid fa-home'
-        
+        to: '/',        
     },
     {
         id: 2,
         title: 'Salah',
         to: '/salah',
-        icon: 'fa-solid fa-person-praying',
     },
     {
         id: 3,
         title: 'Archives',
         exact: true,
         to: '/archives',
-        icon: "fa-brands fa-youtube",
     },
     {
         id: 4,
-        title: 'Announcements',
+        title: 'Service',
         exact: true,
-        to: '/announcements',
-        icon: 'fa-regular fa-calendar',
+        to: '#',
+        subMenus: [
+        { title: "Funeral Information", to: "/funeral" },
+        { title: "Marriage Ceremonies", to: "/marriage",},
+        { title: "Family Counseling", to: "/family",},
+        { title: "Divorce Counseling", to: "/divorce",},
+        { title: "Dawah Program", to: "/dawah",},
+        ]
     },
     {
         id: 5,
-        title: 'About',
+        title: 'Classes',
         exact: true,
-        to: '/about',
-        icon: 'fa-solid fa-circle-info',
+        to: '/classes',
     },
     {
         id: 6,
-        title: 'Contact Us',
+        title: 'Announcements',
         exact: true,
-        to: '/contact',
-        icon: "fa-regular fa-at",
+        to: '/announcements',
     },
+    {
+        id: 7,
+        title: 'Upcoming Events',
+        exact: true,
+        to: '/events',
+    },
+    {
+        id: 8,
+        title: 'About',
+        exact: true,
+        to: '#',
+        subMenus: [
+        { title: "About Us", to: "/about" },
+        { title: "Message from the Imam", to: "/message" },
+        { title: "Contact Us", to: "/contact",},
+        ],
+    },
+    {
+        id: 9,
+        title: 'CEC',
+        exact: true,
+        to: '/cec',
+    },
+    
 ];
 
 const Navbar = (props) => {
     const {global} = props
-   
+    const [subToggle, setSubToggle] = useState(-1)
+
+    const handleSubToggle = (id) => {
+        if(id === subToggle) setSubToggle(-1)
+        else setSubToggle(id)
+    }
+
     return <>
-        <nav className={`sidebar close`}>
-            <ul className="menu-items">
-                {menuItems.map((item) => 
-                    <li key={item.id}>
-                        <div className="block">
-                            <Link to={item.to}>
-                                <i className={`${item.icon}`} style={!global.darkMode ? {color:"#243c64"} : null}></i>
-                            </Link>
-                        </div>
-                        
-                        <ul className="hidden sub-menu">
-                            <Link to={item.to}>
-                                <li key={item.id}>
-                                   <p className={`text-lg transition-all duration-500 ease-linear ${global.darkMode ? 'text-white': 'text-black'}`}>{item.title}</p>
-                                </li>
-                            </Link>
-
-                            {item.subMenus && item.subMenus.map((subItem, index) => <Link to={subItem.to}>
-                                <li id={index} key={index}>
-                                    <p className={`py-1 transition-all duration-300 ease-linear cursor-pointer whitespace-nowrap hover:opacity-100 hover:text-gray-400 ${global.darkMode ? 'text-white': 'text-black'}`}>{subItem.title}</p>
-                                </li> 
-                            </Link>)}
-                        </ul>
-                    </li>
-                )}
-
+        <nav className={`flex justify-evenly pb-1 pt-2 `}>
+            <img src={process.env.PUBLIC_URL + `/logo194.png`} className="w-12" alt="Welcome"/>
+            <ul className="space-x-5">
+                 
+                {menuItems.map((item) => {
+                    return <>
+                        <Link to={item.to}>
+                            <li key={item.id} onClick={() => handleSubToggle(item.id)} className={item.subMenus && 'text-center'}>
+                                <p className={`text-lg ${global.darkMode ? 'text-white': 'text-black'}`}> {item.title}{item.subMenus && <span>&#9662;</span> } </p>
+                                <ul className="dropdown bg-gray-200">
+                                    {item.subMenus && item.subMenus.map((subItem, index) => subToggle === item.id && 
+                                        <li key={index} className={` ${global.darkMode ? 'text-white': 'text-black'}`}>
+                                           <Link to={subItem.to} className="min-w-max">{subItem.title}
+                                           </Link>
+                                        </li> 
+                                    
+                                    )}
+                                </ul>
+                            </li>
+                        </Link>
+                    </> 
+                })}
             </ul>
         </nav>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      display: ['hover', 'focus', 'group-hover']
+    },
   },
   variants: {
     extend: {},
