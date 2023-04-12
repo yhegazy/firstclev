59035774a1e7e8ee94beac4e9ac62f1d444c91a3
yahia <yhegazy@users.noreply.github.com>
2023-04-12 08:56:27 -0400

    Changed gallery from a carousel to a gallery view.

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -1,13 +1,15 @@
 import {useEffect, useState} from 'react'
 import {storage} from '../appwrite/appwriteConfig'
+import { Gallery } from "react-grid-gallery";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
-import Carousel from 'nuka-carousel';
-
-const Galleries = (props) => {
-    const {global} = props
+const Galleries = () => {
     const [gallery, setGallery] = useState({images: []})
     const [isLoading, setIsLoading] = useState(false)
+    const [index, setIndex] = useState(-1);
 
+    //Pull gallery from appwrite
     useEffect(async() => {
         setIsLoading(true)
         const getGallery = await storage.listFiles("images")
@@ -15,15 +17,21 @@ const Galleries = (props) => {
         setIsLoading(false)
     },[])
 
+    //set gallery so lightbox and react grid gallery can read it.
+    const images = []
+    gallery.images.map((item) =>  images.push({src: storage.getFilePreview("images", item.$id)}))
+   
+    const handleClick = (index) => setIndex((index))
+
     return <>
         <div className="sm:w-3/4 w-full ml-auto mr-auto text-2xl p-5 space-y-2 sm:pt-0 pt-24">
             <h1 className="flex justify-center w-1/2 ml-auto mr-auto text-3xl">Gallery</h1>
            
                 {isLoading ? <p>Loading...</p>
-                : <Carousel wrapAround={true} slidesToShow={1} cellSpacing={10}>
-                      {gallery.images.map((item) =>  <img src={storage.getFilePreview("images", item.$id)}/> )}
-                </Carousel>}
-            
+                : [
+                    <Gallery images={images} onClick={(e) => handleClick(e)} enableImageSelection={false}/>,
+                    <Lightbox slides={images} open={index >= 0} index={index} close={() => setIndex(-1)}/>
+                ]}
         </div>
     </>
 }
